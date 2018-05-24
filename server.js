391010e288c6c4ba0bca36fedba9ce93d4cc7423
Sean Bae <seanbae115@gmail.com>
2018-05-23 22:56:26 -0700

    cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const path = require('path');
+const cors = require('cors');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -15,6 +16,7 @@ const app = express();
 // Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 // DB config
 const db = require('./config/credentials').mongoURI;
